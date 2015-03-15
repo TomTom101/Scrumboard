dbbaf9127357e2ae375bf4d5c8428d7e7e364b19
Thomas Brandl <thobra@gmail.com>
2015-03-15 12:39:34 +0100

    Fixed wrong paths

--- __main__.py
+++ __main__.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
 
 import cProfile
-from src import board
+from scrumboard import board
 
 
 def changedStatus(key, status):
     pass
 
 Myboard = board.Board(save_training_file=False)
-Myboard.image = '/Users/thobra/scrumboard/tests/data/board_ni_2.JPG'
+Myboard.image = '/Users/thobra/Dev/scrumboard/tests/data/board_ni_2.JPG'
 Myboard.findLines()
 
 def run():
     global Myboard
-    Myboard.image = '/Users/thobra/scrumboard/tests/data/board_ni_2.JPG'
+    Myboard.image = '/Users/thobra/Dev/scrumboard/tests/data/board_ni_2.JPG'
     Myboard.findCards()
     if Myboard.hasCards():
         print Myboard.list_cards
